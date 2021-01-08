b66b30657efa09166aa2f660113471bca7c539ef
KumarAmitt <amitt2015@outlook.com>
2021-01-08 13:46:32 +0530

    Fix Rubocop offenses

--- main.rb
+++ main.rb
@@ -8,78 +8,83 @@ words = %w[dog door rod blade]
 
 search = proc { |memo, word| memo.length > word.length ? memo : word }
 
-# 1. my_each test-----------------------------
+puts '1. my_each test-----------------------------'
 
-# %w[A B1 c e dad ieee].my_each { |item| puts item.length }
-# a.my_each {|item| print item}
-# puts a.my_each.is_a?(Enumerator)
-# block = proc { |num| num < 4 }
-# print a.my_each(&block)
-# print range.my_each(&block)
+%w[A B1 c e dad ieee].my_each { |item| puts item.length }
+a.my_each { |item| print item }
+puts a.my_each.is_a?(Enumerator)
+block = proc { |num| num < 4 }
+print a.my_each(&block)
+print range.my_each(&block)
 
+puts "\n\n"
+puts '2. my_each_with_index test-----------------------------'
 
+%w[any k a b c d e].my_each_with_index { |item, index| puts "#{item} : #{index}" }
+a.my_each_with_index { |item, index| puts "#{item} & #{index}" }
+puts a.my_each_with_index.is_a?(Enumerator)
+block = proc { |num| num < 4 }
+print a.my_each_with_index(&block)
 
-#  2 ***my_each_with_index
+puts "\n\n"
+puts '3. my_select test-----------------------------'
 
-#  ["any","k","a","b","c","d","e"].my_each_with_index { |item,index| puts "#{item} : #{index}"}
-#  a.my_each_with_index {|item,index| puts "#{item} & #{index}"}
-#  puts a.my_each_with_index.is_a?(Enumerator)
-#  block = proc { |num| num < 4 }
-#  print a.my_each_with_index(&block)
+print(a.my_select(&:even?))
+print a.my_select.is_a?(Enumerator)
 
-# 3 *** my_select test---------------------------
+puts "\n\n"
+puts '4. my_all? test-----------------------------'
 
-# print(a.my_select(&:even?))
-# print a.my_select.is_a?(Enumerator)
+puts %w[ant bear cat].my_all? { |word| word.length >= 3 } #=>true
+puts %w[ant bear cat].my_all? { |word| word.length >= 4 } #=>false
+puts %w[ant bear cat].my_all?(/t/) #=>false
+puts [1, 2i, 3.14].my_all?(Numeric) #=>true
+puts [nil, true, 99].my_all? #=>false
+puts [].my_all? #=>true
+puts a.my_all?(3) #=>false
+puts [1, 'hi', true, []].my_all? #=>true
 
-# 4 *** my_all? test-----------------------------
-
-# puts %w[ant bear cat].my_all? { |word| word.length >= 3 }  #=>true
-# puts %w[ant bear cat].my_all? { |word| word.length >= 4 }  #=>false
-# puts %w[ant bear cat].my_all?(/t/)                    #=>false
-# puts [1, 2i, 3.14].my_all?(Numeric)                        #=>true
-# puts [nil, true, 99].my_all?                               #=>false
-# puts [].my_all?                                            #=>true
-# puts a.my_all?(3)                                    #=>false
-# puts [1, 'hi', true, []].my_all?                           #=>true
-
-# 5 *** my_any? test-----------------------------------
-
-
-# puts %w[ant bear cat].my_any? { |word| word.length >= 3 } #=> true
-# puts %w[ant bear cat].my_any? { |word| word.length >= 4 } #=> true
-# puts %w[ant bear cat].my_any?(/d/)                        #=> false
-# puts [nil, true, 99].my_any?(Integer)                     #=> true
-# puts [nil, true, 99].my_any?                              #=> true
-# puts [].my_any?                                           #=> false
-# puts words.my_any?('cat')                                 #=> false
-# puts [  nil, false].my_any?                               #=> false
-
-# # 6 *** my_none? test
-
-# puts %w{ant bear cat}.my_none? { |word| word.length == 5 } #=> true
-# puts %w{ant bear cat}.my_none? { |word| word.length >= 4 } #=> false
-# puts %w{ant bear cat}.my_none?(/d/)                        #=> true
-# puts [1, 3.14, 42].my_none?(Float)                         #=> false
-# puts [].my_none?                                           #=> true
-# puts [nil].my_none?                                        #=> true
-# puts [nil, false].my_none?                                 #=> true
-# puts [nil, false, true].my_none?                           #=> false
-# puts words.my_none?(5)                                     #=> true
-# puts [3, 4, 7, 11].my_none?(4)                             #=> false
+puts "\n\n"
+puts '5. my_any? test-----------------------------'
 
 
+puts %w[ant bear cat].my_any? { |word| word.length >= 3 } #=> true
+puts %w[ant bear cat].my_any? { |word| word.length >= 4 } #=> true
+puts %w[ant bear cat].my_any?(/d/) #=> false
+puts [nil, true, 99].my_any?(Integer) #=> true
+puts [nil, true, 99].my_any? #=> true
+puts [].my_any? #=> false
+puts words.my_any?('cat') #=> false
+puts [nil, false].my_any? #=> false
 
-# # 7 *** my_count? test
+puts "\n\n"
+puts '6. my_none? test-----------------------------'
 
-# ary = [1, 2, 4, 2]
-# puts ary.my_count               #=> 4
-# puts ary.my_count(2)            #=> 2
-# puts ary.my_count{ |x| x%2==0 } #=> 3
-# puts range.my_count             #=> 46
+puts %w[ant bear cat].my_none? { |word| word.length == 5 } #=> true
+puts %w[ant bear cat].my_none? { |word| word.length >= 4 } #=> false
+puts %w[ant bear cat].my_none?(/d/) #=> true
+puts [1, 3.14, 42].my_none?(Float) #=> false
+puts [].my_none? #=> true
+puts [nil].my_none? #=> true
+puts [nil, false].my_none? #=> true
+puts [nil, false, true].my_none? #=> false
+puts words.my_none?(5) #=> true
+puts [3, 4, 7, 11].my_none?(4) #=> false
 
 
-# # 8 *** my_map test
+
+puts "\n\n"
+puts '7. my_count test-----------------------------'
+
+ary = [1, 2, 4, 2]
+puts ary.my_count #=> 4
+puts ary.my_count(2) #=> 2
+puts(ary.my_count(&:even?)) #=> 3
+puts range.my_count #=> 46
+
+
+puts "\n\n"
+puts '8. my_map test-----------------------------'
 
 print (1..4).my_map { |i| i * i } #=> [1, 4, 9, 16]
 random_proc = proc { |num| num * 2 }
@@ -88,34 +93,30 @@ puts a.my_map.is_a?(Enumerator) #=> true
 my_proc = proc { |num| num > 10 }
 print a.my_map(my_proc) { |num| num < 10 }
 
+puts "\n\n"
+puts '9. my_inject test-----------------------------'
 
+# Sum some numbers
+puts (5..10).my_inject(:+) #=> 45
+# Same using a block and inject
+puts (5..10).my_inject { |sum, n| sum + n } #=> 45
+# Multiply some numbers
+puts (5..10).my_inject(1, :*) #=> 151200
+# Same using a block
+puts (5..10).my_inject(1) { |product, n| product * n } #=> 151200
 
+puts range.my_inject(:*)
+puts range.my_inject(2, :*)
 
-# # 9 *** my_inject test
+search = proc { |memo, word| memo.length > word.length ? memo : word }
+words = %w[dog doooooor rod blade]
 
-#Sum some numbers
-# puts (5..10).my_inject(:+)                             #=> 45
-# # Same using a block and inject
-# puts (5..10).my_inject { |sum, n| sum + n }            #=> 45
-# # Multiply some numbers
-# puts (5..10).my_inject(1, :*)                          #=> 151200
-# # Same using a block
-# puts (5..10).my_inject(1) { |product, n| product * n } #=> 151200
-#
-# puts range.my_inject(:*)
-# puts range.my_inject(2,:*)
-#
-# search = proc { |memo, word| memo.length > word.length ? memo : word }
-# words = ["dog", "doooooor", "rod", "blade"]
-#
-#
-# puts words.my_inject(&search)
+puts words.my_inject(&search)
 
+puts "\n\n"
+puts '10. multiply_els test-----------------------------'
 
-
-# # 10 *** multiply_els test
-
-puts multiply_els([2,4,5])
+puts multiply_els([2, 4, 5])
 
 
 
