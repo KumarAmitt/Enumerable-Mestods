f65e212f13d3f403a7f5441fd6aa2d54b73d9c77
KumarAmitt <amitt2015@outlook.com>
2021-01-20 15:34:14 +0530

    Fix RuboCop Offenses

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -1,10 +1,11 @@
 require_relative '../lib/enumerable'
+require 'rspec'
 
 RSpec.describe Enumerable do
-  let(:a) {[5, 3, 2, 4]}
-  let(:range) {(1..8)}
-  let(:words) {%w[dog door rod blade]}
-  let(:block) { proc { |num| num < 4 }}
+  let(:a) { [5, 3, 2, 4] }
+  let(:range) { (1..8) }
+  let(:words) { %w[dog door rod blade] }
+  let(:block) { proc { |num| num < 4 } }
 
   describe '#my_each' do
     it 'should returns an Enumerator when no block given' do
@@ -12,7 +13,7 @@ RSpec.describe Enumerable do
     end
 
     it 'should return the original array' do
-      expect(words.my_each {|e| e}) .to eq(words)
+      expect(words.my_each { |e| e }).to eq(words)
     end
 
     it 'prints result to stdout from block calculation' do
@@ -30,7 +31,7 @@ RSpec.describe Enumerable do
     end
 
     it 'prints the element and corresponding index at stdout' do
-      expect { a.my_each_with_index { |el, i| print el, i }}.to output(/50312243/).to_stdout
+      expect { a.my_each_with_index { |el, i| print el, i } }.to output(/50312243/).to_stdout
     end
 
     it 'returns original array when no output' do
@@ -47,7 +48,7 @@ RSpec.describe Enumerable do
       expect(a.my_select(&:even?)).to eq([2, 4])
     end
 
-    it 'should return a new array where the elements are elements of the original array where the block resulted in true' do
+    it 'should return an array where the elements are elements of the original array where the block result in true' do
       expect(a.my_select(&block)).to eq([3, 2])
     end
   end
@@ -76,13 +77,13 @@ RSpec.describe Enumerable do
     it 'should return false' do
       expect(a.my_none?(&:odd?)).to be_falsy
     end
-  
+
     it 'should return true' do
       expect(a.my_none? { |n| n > 10 }).to be_truthy
     end
 
     it 'should return false' do
-      expect([true,false,nil].my_none?).to be_falsy
+      expect([true, false, nil].my_none?).to be_falsy
     end
 
     it 'should return true' do
@@ -96,7 +97,6 @@ RSpec.describe Enumerable do
     it 'should return false' do
       expect(words.my_none?(/d/)).to be_falsy
     end
-
   end
 
   describe '#my_count?' do
@@ -112,40 +112,32 @@ RSpec.describe Enumerable do
       expect(words.my_count).to eq(4)
     end
 
-    it 'returns the number of items that match the positional argument given' do 
+    it 'returns the number of items that match the positional argument given' do
       expect(words.my_count('rod')).to eq(1)
     end
   end
 
   describe '#my_map' do
-
     it 'returns a modified array resulting from block operation' do
       expect(a.my_map { |n| 2 * n }).to eq([10, 6, 4, 8])
     end
 
     it 'returns modified array when given a range with a block' do
-      expect(range.my_map { |n| n+2}).to eq([3,4,5,6,7,8,9,10])
-    end
-    
-    it 'returns array of strings when given array of integers' do
-      expect(a.my_map(&:to_s)).to eq(['5', '3', '2', '4'])
+      expect(range.my_map { |n| n + 2 }).to eq([3, 4, 5, 6, 7, 8, 9, 10])
     end
 
+    it 'returns array of strings when given array of integers' do
+      expect(a.my_map(&:to_s)).to eq(%w[5 3 2 4])
+    end
   end
 
   describe '#my_inject' do
-
     it 'returns the accumulative result when given a starting value and a block operation' do
       expect(a.my_inject(1) { |product, n| product * n }).to eq(120)
     end
 
-    it 'returns the accumulative result when given starting value and a urnary operator' do
-      expect(range.my_inject(1, :*)).to eq(40320)
+    it 'returns the accumulative result when given starting value and a unary operator' do
+      expect(range.my_inject(1, :*)).to eq(40_320)
     end
-
-   
   end
-
 end
-
-
