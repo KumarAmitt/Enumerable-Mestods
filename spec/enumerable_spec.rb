3bdaf10815ee5028ba080b460d899e443fb40c24
saimaAnis21 <saima_rehman21@yahoo.com>
2021-01-20 14:53:28 +0500

    Add Test conditions for my_none, my_count, my_map and my_inject

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -2,7 +2,7 @@ require_relative '../lib/enumerable'
 
 RSpec.describe Enumerable do
   let(:a) {[5, 3, 2, 4]}
-  let(:range) {Range.new(5..10)}
+  let(:range) {(1..8)}
   let(:words) {%w[dog door rod blade]}
   let(:block) { proc { |num| num < 4 }}
 
@@ -63,14 +63,89 @@ RSpec.describe Enumerable do
   end
 
   describe '#my_any?' do
-  it 'should return true' do
-    expect(a.my_any?(&:odd?)).to be_truthy
+    it 'should return true' do
+      expect(a.my_any?(&:odd?)).to be_truthy
+    end
+
+    it 'should return false' do
+      expect(a.my_any? { |n| n > 10 }).to be_falsy
+    end
   end
 
-  it 'should return false' do
-    expect(a.my_any? { |n| n > 10 }).to be_falsy
-  end
+  describe '#my_none?' do
+    it 'should return false' do
+      expect(a.my_none?(&:odd?)).to be_falsy
+    end
+  
+    it 'should return true' do
+      expect(a.my_none? { |n| n > 10 }).to be_truthy
+    end
+
+    it 'should return false' do
+      expect([true,false,nil].my_none?).to be_falsy
+    end
+
+    it 'should return true' do
+      expect([].my_none?).to be_truthy
+    end
+
+    it 'should return true' do
+      expect(words.my_none?(/x/)).to be_truthy
+    end
+
+    it 'should return false' do
+      expect(words.my_none?(/d/)).to be_falsy
+    end
+
   end
 
+  describe '#my_count?' do
+    it 'should return the total no of elements in the array' do
+      expect(a.my_count).to eq(4)
+    end
+
+    it 'should return the total of odd elements in the array' do
+      expect(a.my_count(&:odd?)).to eq(2)
+    end
+
+    it 'returns array length when no block given' do
+      expect(words.my_count).to eq(4)
+    end
+
+    it 'returns the number of items that match the positional argument given' do 
+      expect(words.my_count('rod')).to eq(1)
+    end
+  end
+
+  describe '#my_map' do
+
+    it 'returns a modified array resulting from block operation' do
+      expect(a.my_map { |n| 2 * n }).to eq([10, 6, 4, 8])
+    end
+
+    it 'returns modified array when given a range with a block' do
+      expect(range.my_map { |n| n+2}).to eq([3,4,5,6,7,8,9,10])
+    end
+    
+    it 'returns array of strings when given array of integers' do
+      expect(a.my_map(&:to_s)).to eq(['5', '3', '2', '4'])
+    end
+
+  end
+
+  describe '#my_inject' do
+
+    it 'returns the accumulative result when given a starting value and a block operation' do
+      expect(a.my_inject(1) { |product, n| product * n }).to eq(120)
+    end
+
+    it 'returns the accumulative result when given starting value and a urnary operator' do
+      expect(range.my_inject(1, :*)).to eq(40320)
+    end
+
+   
+  end
 
 end
+
+
